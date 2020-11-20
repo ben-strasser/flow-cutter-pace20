9982fd02239e07a9ab574dc71e604aae93253179
Ben Strasser <github@ben-strasser.net>
2020-11-20 21:55:21 +0100

    added missing include_all.cpp file

---
+++ src/include_all.cpp
@@ -0,0 +1,7 @@
+// This file exists only to get CMake to compile everything in one call to g++
+// to enable inlining across translation unit boundaries.
+#include "greedy_order.cpp" 
+#include "pace.cpp" 
+#include "list_graph.cpp"
+#include "tree_depth_decomposition.cpp"
+
